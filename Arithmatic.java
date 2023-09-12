92fe4a339d882b1c02ca4510f9815b8cb8e8fa7b
Nandinikshirsagar <144772911+Nandinikshirsagar@users.noreply.github.com>
2023-09-12 16:03:57 +0530

    Add files via upload
    java codes

---
+++ Arithmatic.java
@@ -0,0 +1,18 @@
+public class Arithmatic
+{
+public static void main(String args[])
+{
+System.out.println("My First Program is Java");
+int a=122, b=101;
+int add,sub,mul,div;
+add = a+b;
+sub = a-b;
+mul = a*b;
+div = a/b;
+System.out.println("Addition : "+ add);
+System.out.println("substaction :"+ sub);
+System.out.println("Multipication :"+ mul);
+System.out.println("Division :"+div);
+
+}
+}
---
+++ FirstProgram.java
@@ -0,0 +1,15 @@
+
+public class FirstProgram
+{
+public static void main(String args[])
+{
+System.out.println("My First Program in Java");
+int a= 88, b= 90;
+int c;
+c = a+b;
+system.out.println("c");
+
+
+}
+}
+
